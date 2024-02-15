4fd5033ee1dc7acd5d4a20a6b585bdd0f8f2db26
LinElijah <23441313@life.hkbu.edu.hk>
2024-02-15 12:57:27 +0800

    data cleaning script

---
+++ clean.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Input first file
+contact_df = pd.read_csv('respondent_contact.csv')
+# Input second file
+other_df = pd.read_csv('respondent_other.csv')
+
+# Merge the two dataframes based on the ID column
+merged_df = pd.merge(contact_df, other_df, left_on = 'respondent_id', right_on = 'id')
+# Delete id column to avoid redundancy
+merged_df = merged_df.drop('id', axis = 1)
+
+# Drop any rows with missing values
+merged_df = merged_df.dropna()
+
+# Drop rows with 'job' value containing 'insurance' or 'Insurance'
+merged_df = merged_df[~merged_df['job'].str.contains('insurance|Insurance')]
+
+# Save the modified data to a new CSV file
+merged_df.to_csv('clean_data.csv', index=False)
